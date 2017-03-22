88f319fbad7e055d28f93524bdb231c7e5867958
Marcin Chmielewski <marcin.chmielewski@hycom.pl>
2017-03-22 16:49:09 +0100

    Speed buttons added

--- Nawilzacz.ino
+++ Nawilzacz.ino
@@ -6,9 +6,10 @@
  * network topology allowing messages to be routed to nodes.
  *
  * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Refactored and addjusted by Marcin Chmielewski <marcin.chmielewski@gmail.com>
+ * Refactored and addjusted for Air humidifier by Marcin Chmielewski <marcin.chmielewski@gmail.com>
  * Copyright (C) 2013-2015 Sensnology AB & 2017 Hopfen87
  * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ * The letest varsion of Air Humidifier is available here: https://github.com/hopfen87/Nawilzacz
  *
  * Documentation: http://www.mysensors.org
  * Support Forum: http://forum.mysensors.org
@@ -24,9 +25,9 @@
  * Version 1.1 - Marcin Chmielewski
  * 
  * DESCRIPTION
- * This sketch provides an example how to implement a humidity/temperature
- * sensor using DHT11/DHT-22 
- * http://www.mysensors.org/build/humidity
+ * This sketch provides full automation of air humidifier. Controls humidity and temperature by DHT-22 sensor
+ * and allows remote control of Stadler Form Oscar Little humidifier.
+ * 
  */
 
 #define MY_NODE_ID 9
@@ -47,10 +48,20 @@
 unsigned long SLEEP_TIME = 30000; // Sleep time between reads (in milliseconds)
 unsigned long lastRefreshTime = 0; // Use this to implement a non-blocking delay function
 
-#define RELAY_1  4  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
-#define NUMBER_OF_RELAYS 1 // Total number of attached relays
-#define RELAY_ON 1  // GPIO value to write to turn on attached relay
-#define RELAY_OFF 0 // GPIO value to write to turn off attached relay
+#define RELAY_1  4  // Arduino Digital I/O pin number for first relay PIN 4 for POWER BUTTON; PIN 5 for SPEED BUTTON; PIN 6 for DIMMER BUTTON
+#define NUMBER_OF_RELAYS 3 // Total number of attached relays 
+#define RELAY_ON 1  // GPIO value to write to simulate not pressed button
+#define RELAY_OFF 0 // GPIO value to write to simulate pressed button
+
+#define SPEED1_SENSOR_PIN 7
+#define SPEED2_SENSOR_PIN 8
+#define LIGHT_SENSOR_PIN  14
+#define WATER_SENSOR_PIN  15
+
+#define SPEED1_SENSOR_CHILD_ID 7
+#define SPEED2_SENSOR_CHILD_ID 8
+#define LIGHT_SENSOR_CHILD_ID  14
+#define WATER_SENSOR_CHILD_ID  15
 
 
 
@@ -58,8 +69,14 @@ DHT dht;
 float lastTemp;
 float lastHum;
 boolean metric = true; 
+int speed1OldValue = -1;
+int speed2OldValue = -1;
+
+
 MyMessage msgHum(CHILD_ID_HUM, V_HUM);
 MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
+MyMessage msgSpeed1(SPEED1_SENSOR_CHILD_ID, V_STATUS);
+MyMessage msgSpeed2(SPEED2_SENSOR_CHILD_ID, V_STATUS);
 
 
 void setup()  
@@ -71,15 +88,18 @@ void setup()
   for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
     // Then set relay pins in output mode
     pinMode(pin, OUTPUT);   
-    // Set relay to last known state (using eeprom storage) 
-    digitalWrite(pin, loadState(sensor)?RELAY_OFF:RELAY_ON);
+    // Set relay (push button) default value
+    digitalWrite(pin, RELAY_ON);
   }
+    pinMode(SPEED1_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(SPEED2_SENSOR_PIN, INPUT_PULLUP);
+
 }
 
 void presentation()  
 { 
   // Send the Sketch Version Information to the Gateway
-  sendSketchInfo("My Bedroom", "1.0");
+  sendSketchInfo("Nawilżacz", "1.1");
 
   // Register all sensors to gw (they will be created as child devices)
   present(CHILD_ID_HUM, S_HUM);
@@ -89,6 +109,9 @@ void presentation()
     // Register all sensors to gw (they will be created as child devices)
     present(sensor, S_BINARY);
   }
+
+  present(SPEED1_SENSOR_CHILD_ID, S_BINARY);
+  present(SPEED2_SENSOR_CHILD_ID, S_BINARY);
 }
 
 
@@ -123,7 +146,19 @@ void loop()
       }
 
   }
-  
+
+  int speed1SensorVal = digitalRead(SPEED1_SENSOR_PIN);
+  if (speed1SensorVal != speed1OldValue) {
+     // Send in the new value
+     send(msgSpeed1.set(speed1SensorVal==HIGH ? 1 : 0));
+     speed1OldValue = speed1SensorVal;
+  }
+  int speed2SensorVal = digitalRead(SPEED2_SENSOR_PIN);
+  if (speed2SensorVal != speed2OldValue) {
+     // Send in the new value
+     send(msgSpeed2.set(speed2SensorVal==HIGH ? 1 : 0));
+     speed2OldValue = speed2SensorVal;
+  }
 }
 
 void receive(const MyMessage &message) {
@@ -131,8 +166,8 @@ void receive(const MyMessage &message) {
   if (message.type==V_STATUS) {
      // Change relay state
      digitalWrite(message.sensor-1+RELAY_1, message.getBool()?RELAY_OFF:RELAY_ON);
-     // Store state in eeprom
-     saveState(message.sensor, message.getBool());
+     // Store state in eeprom - saving state is not necessary
+     // saveState(message.sensor, message.getBool());
      // Write some debug info
      Serial.print("Incoming change for sensor:");
      Serial.print(message.sensor);
